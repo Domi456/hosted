b9ce2c65945b4410be05e55e7bd91d5a4d991c8c
Domi456 <121254412+Domi456@users.noreply.github.com>
2025-05-10 07:34:02 +0200

    sdgdg

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/hosted/",
   plugins: [tailwindcss(), react()],
-  base: "/hosted",
 })
